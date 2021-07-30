22f4ccdf67507a622e0ca4637b9f9b729e21a892
jpurbia <jonty_p@itra.co.jp>
2021-07-30 22:30:47 +0530

    Add User and Message relationships

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -10,4 +10,13 @@ class Message extends Model
     use HasFactory;
 
     protected $fillable = ['message'];
+
+    /**
+     *  A message belong to a user
+     * 
+     *  @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
